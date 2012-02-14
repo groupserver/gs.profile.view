9e8dc2f57cc7101a283fb01e2a0d9700910454dc
Michael JasonSmith <mpj17@onlinegroups.net>
2012-02-14 16:25:51 +1100

    Making the profile viewlet for the site homepage work

--- gs/profile/view/sitehomesummary.py
+++ gs/profile/view/sitehomesummary.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from gs.viewlet.viewlet import SiteViewlet
 from zope.cachedescriptors.property import Lazy
+from zope.component import createObject
+from Products.GSGroup.groupInfo import GSGroupInfoFactory
 
 # I know the base-class may seem strange, but bare with me. This viewlet
 #   is shown on the site homepage. It is not shown in the context of
@@ -15,4 +17,21 @@ class SiteHomeSummaryViewlet(SiteViewlet):
     @Lazy
     def userInfo(self):
         return self.loggedInUser
+    
+    @Lazy
+    def salutation(self):
+        retval = self.context.DivisionConfiguration.get('greeting','Welcome')
+        return retval
+
+    @Lazy
+    def groups(self):
+      retval = createObject('groupserver.GroupsInfo', self.context)
+      return retval
+
+    @Lazy
+    def memberGroups(self):
+        u = self.userInfo.user
+        mg = self.groups.get_member_groups_for_user(u, u)
+        retval = [createObject('groupserver.GroupInfo', g) for g in mg]
+        return retval
 
